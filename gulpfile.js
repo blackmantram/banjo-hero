b43425216b466df84412a31b6d7f50a84201fe67
Sergio Alexander Rico Abella <sergio.rico@PRAGMA.COM.CO>
2016-08-04 13:59:56 -0500

    app folder structure & serve task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('default', function(){
-	
+gulp.task('serve', function(){
+	browserSync({
+		server:{
+			baseDir: 'app'
+		}
+	});
+
+	gulp.watch(['*.html', 'styles/**/*.css', 'js/**/*.js'], {cwd: 'app'}, reload)
 });
